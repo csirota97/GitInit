7118b2c82bbeb6fcc7a453fbad875aa7f8d771c1
Craig Sirota <34724663+csirota97@users.noreply.github.com>
2023-04-19 12:47:24 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,24 +1,32 @@
 if [ -z $1 ]; then
     echo 'Provide project name'
     exit;
+else
+    localRepoName=$1
+    localRepoName=${localRepoName// /_}
 fi
+
+if [ -z $2 ]; then
+    remoteRepoName=$1
+else
+    remoteRepoName=$2
+fi
+
 if [ ${#GITUSER} -eq 0 ]; then
-    echo 'Missing Github Username!'
+    echo 'NEED NAME'
     read -p "Enter Github Username: " GITUSER
 else
     echo $GITUSER
 fi
 
-echo 'Initializing Project' $1
-string=$1
-string=${string// /_}
+echo 'Initializing Project' $localRepoName
 
-mkdir $string $string/src
-echo '#' $1 > $string/README.md
-cd $string
+mkdir $localRepoName $localRepoName/src
+cd $localRepoName
+echo '#' $remoteRepoName > ./README.md
 pwd
 git init
 git add README.md
 git commit -m 'Initial Commit'
-git remote add origin 'https://github.com/'$GITUSER'/'$string'.git'
+git remote add origin 'https://github.com/'$GITUSER'/'$remoteRepoName'.git'
 git push -u origin master
